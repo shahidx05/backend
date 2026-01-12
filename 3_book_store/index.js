cbc7b635096ad11b3de340b58dafd75055ef5bff
Shahid Khan <shahidjoura307@gmail.com>
2026-01-12 21:08:02 +0530

    done book

--- 3_book_store/index.js
+++ 3_book_store/index.js
@@ -8,7 +8,8 @@ app.use(express.json())
 const BookStore = [
     { id: 1, name: "Harry Potter", author: "DevFIux" },
     { id: 2, name: "Friends", author: "Vikas" },
-    { id: 3, name: "Nexus", author: "Roht" }
+    { id: 3, name: "Nexus", author: "Rohit" },
+    { id: 4, name: "Nex", author: "Rohit" }
 ]
 
 app.get('/', (req, res) => {
@@ -16,7 +17,18 @@ app.get('/', (req, res) => {
 })
 
 app.get('/book', (req, res) => {
-    res.send(BookStore)
+    // res.send(BookStore) old
+
+    let result = BookStore
+    const { author } = req.query
+
+    if (author) {
+        result = result.filter(book =>
+            book.author.toLowerCase().includes(author.toLowerCase())
+        )
+    }
+
+    res.send(result)
 })
 
 app.get('/book/:id', (req, res) => {
@@ -35,18 +47,24 @@ app.post('/book', (req, res) => {
 app.patch('/book', (req, res) => {
     const book = BookStore.find(e=>e.id==req.body.id)
     if(!book) return res.send("not found")
-    book = req.body
+    if(req.body.name) book.name = req.body.name
+    if(req.body.author) book.author = req.body.author
     res.send(book)
 })
 
-app.put('/book/:id', (req, res) => {
-    const {id} = req.params
-
-    console.log(req.body) 
-    BookStore.push(req.body)
-    res.send(req.body)
+app.put('/book', (req, res) => {
+    const book = BookStore.find(e=>e.id==req.body.id)
+    if(!book) return res.send("not found")
+     book.author = req.body.author
+    book.name = req.body.name
+    res.send(book)
 })
 
+app.delete('/book/:id', (req, res)=>{
+    const idx = BookStore.findIndex(e=>e.id==req.params.id)
+    BookStore.splice(idx, 1)
+    res.send("delete successfully")
+})
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
